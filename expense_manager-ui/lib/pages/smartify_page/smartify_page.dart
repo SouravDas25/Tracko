d335213744f8e805b081490df43c0c2c3e98e574
souravdas25 <souravbumbadas25@gmail.com>
2019-03-16 22:57:23 +0530

    smartify frontend completed

--- expense_manager-ui/lib/pages/smartify_page/smartify_page.dart
+++ expense_manager-ui/lib/pages/smartify_page/smartify_page.dart
@@ -18,12 +18,16 @@ import 'package:flushbar/flushbar.dart';
 * Text Razor APi KEY - 6a72f94bb1182f454256c0591e99e75dcc630bdb8054f2fa05edd15b
 * */
 
+enum ScanningStatus { NOT_RUNNING, RUNNING, COMPLETED }
+
 class SmartPage extends StatefulWidget {
   List<PossibleTransaction> possibleTransactions = SmartUtil.getPT();
-  bool scanning = false;
+  ScanningStatus scanning = ScanningStatus.NOT_RUNNING;
 
   SmartPage() {
-    if (possibleTransactions.length > 0) this.scanning = true;
+    if (possibleTransactions.length > 0) {
+      this.scanning = ScanningStatus.COMPLETED;
+    }
   }
 
   @override
@@ -51,7 +55,7 @@ class _SmartPage extends State<SmartPage> {
       duration: Duration(seconds: 200),
       mainButton: FlatButton(
         onPressed: () {
-          this.cancle();
+          this.cancel();
         },
         child: Text(
           "Cancle",
@@ -61,30 +65,11 @@ class _SmartPage extends State<SmartPage> {
     );
   }
 
-  void cancle() {
+  void cancel() {
     flushbar.dismiss();
     widget.possibleTransactions.clear();
-    widget.scanning = false;
+    widget.scanning = ScanningStatus.NOT_RUNNING;
     setState(() {});
-  }
-
-  Future<PossibleTransaction> getApiData(String message) async {
-    var url = "http://souravdas25.pythonanywhere.com/apis/?text=$message";
-
-    // Await the http get response, then decode the json-formatted responce.
-    var response = await http.get(url);
-    if (response.statusCode == 200) {
-      var jsonResponse = convert.jsonDecode(response.body);
-      print(jsonResponse);
-      PossibleTransaction transaction =
-          PossibleTransaction.fromJson(jsonResponse);
-      if (transaction.valid) {
-        return transaction;
-      }
-      return null;
-    } else {
-      return null;
-    }
   }
 
   void processMessages(List<SmsMessage> messages) async {
@@ -102,12 +87,12 @@ class _SmartPage extends State<SmartPage> {
     }
     setState(() {
       flushbar.dismiss();
-//      scanning = false;
+      widget.scanning = ScanningStatus.COMPLETED;
     });
   }
 
   void initData() async {
-    widget.scanning = true;
+    widget.scanning = ScanningStatus.RUNNING;
     List<SmsMessage> messages = await query.querySms(count: 20);
     processMessages(messages);
     flushbar.show(context);
@@ -115,37 +100,39 @@ class _SmartPage extends State<SmartPage> {
 
   void dismissAll() {
     widget.possibleTransactions.clear();
-    widget.scanning = false;
+    widget.scanning = ScanningStatus.NOT_RUNNING;
     setState(() {});
   }
 
-  void saveAll() {
-
-  }
+  void saveAll() {}
 
   @override
   Widget build(BuildContext context) {
-    if (widget.scanning) {
+    if (widget.scanning == ScanningStatus.RUNNING ||
+        widget.scanning == ScanningStatus.COMPLETED) {
       return ListView(
         children: widget.possibleTransactions
             .map<Widget>((PossibleTransaction transaction) {
-          return Card(
-            child: ListTile(
-              onTap: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => SmartAddItemPage(transaction)));
-              },
-              contentPadding: EdgeInsets.all(10.0),
-              title: Text(
-                transaction.category == null
-                    ? "Item "
-                    : transaction.category.name,
-                style: TextStyle(fontSize: 20.0),
-              ),
-              subtitle: Text(CommonUtil.humanDate(transaction.dates[0])),
-              trailing: Text(
-                "₹ " + transaction.amounts[0].toString(),
-                style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
+          return IgnorePointer(
+            ignoring: widget.scanning == ScanningStatus.RUNNING ? true : false,
+            child: Card(
+              child: ListTile(
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => SmartAddItemPage(transaction)));
+                },
+                contentPadding: EdgeInsets.all(10.0),
+                title: Text(
+                  transaction.category == null
+                      ? "Item "
+                      : transaction.category.name,
+                  style: TextStyle(fontSize: 20.0),
+                ),
+                subtitle: Text(CommonUtil.humanDate(transaction.dates[0])),
+                trailing: Text(
+                  "₹ " + transaction.amounts[0].toString(),
+                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
+                ),
               ),
             ),
           );
@@ -155,27 +142,37 @@ class _SmartPage extends State<SmartPage> {
                 child: Row(
                   children: <Widget>[
                     Expanded(
-                      child: RaisedButton(
-                        padding: EdgeInsets.all(20.0),
-                        color: Colors.green,
-                        textColor: Colors.white,
-                        onPressed: () {},
-                        child: Text(
-                          "Save All",
-                          style: TextStyle(fontSize: 20.0),
+                      child: IgnorePointer(
+                        ignoring: widget.scanning == ScanningStatus.RUNNING
+                            ? true
+                            : false,
+                        child: RaisedButton(
+                          padding: EdgeInsets.all(20.0),
+                          color: Colors.green,
+                          textColor: Colors.white,
+                          onPressed: () {},
+                          child: Text(
+                            "Save All",
+                            style: TextStyle(fontSize: 20.0),
+                          ),
                         ),
                       ),
                     ),
                     Container(
-                      child: RaisedButton(
-                        padding: EdgeInsets.all(20.0),
-                        color: Colors.red,
-                        onPressed: () {
-                          dismissAll();
-                        },
-                        child: Icon(
-                          Icons.delete,
-                          color: Colors.white,
+                      child: IgnorePointer(
+                        ignoring: widget.scanning == ScanningStatus.RUNNING
+                            ? true
+                            : false,
+                        child: RaisedButton(
+                          padding: EdgeInsets.all(20.0),
+                          color: Colors.red,
+                          onPressed: () {
+                            dismissAll();
+                          },
+                          child: Icon(
+                            Icons.delete,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                     )
@@ -207,5 +204,24 @@ class _SmartPage extends State<SmartPage> {
         )
       ],
     );
+  }
+}
+
+Future<PossibleTransaction> getApiData(String message) async {
+  var url = "http://souravdas25.pythonanywhere.com/apis/?text=$message";
+
+  // Await the http get response, then decode the json-formatted responce.
+  var response = await http.get(url);
+  if (response.statusCode == 200) {
+    var jsonResponse = convert.jsonDecode(response.body);
+    print(jsonResponse);
+    PossibleTransaction transaction =
+        PossibleTransaction.fromJson(jsonResponse);
+    if (transaction.valid) {
+      return transaction;
+    }
+    return null;
+  } else {
+    return null;
   }
 }
