e4201046681a30358e9144ace274f13048346763
I353584 <sourav.das01@sap.com>
2019-04-07 13:52:05 +0530

    java backend SCP compatible

---
+++ java-backend/src/main/java/com/expense/manager/MainController.java
@@ -0,0 +1,17 @@
+package com.expense.manager;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class MainController {
+	
+	@RequestMapping(name="/demo")
+	@ResponseBody
+	public String demo() {
+		return "Hello World";
+	}
+
+}
